ba93e8a4f965623cc80176a4173cbaa57157aec5
Ethan Arterberry <ethanarterberry@gmail.com>
2015-07-01 10:55:46 -0700

    begin working

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>quickiebin</title>
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+    Language: <input type="text" class="language" id="lang">
+    Code:
+    <br>
+    <textarea class="code" id="code"></textarea>
+  </body>
+</html>
